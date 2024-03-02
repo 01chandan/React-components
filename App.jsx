e5ea344f849062ca971b4b8119a99ebb4d5ef32c
CHANDAN KUMAR <121013050+01chandan@users.noreply.github.com>
2024-03-02 20:31:20 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import react from 'react';
+import Card from './components/Card';
+import Cart2 from './components/Cart2';
+import HandlingEvents from './components/HandlingEvents';
+function App(){
+  return(
+    <>
+        <HandlingEvents/>
+    </>
+  )
+}
+
+export default App;
